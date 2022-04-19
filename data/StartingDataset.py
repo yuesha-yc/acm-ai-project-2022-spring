43d0e8e1b5722692b97cb90a8e52067e597af857
Derek Hua <derekhua@ucla.edu>
2022-04-18 19:57:58 -0700

    Created simple loading code for training and testing sets, has bug in starting_train.py

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,4 +1,10 @@
 import torch
+from PIL import Image
+import torchvision.transforms as transforms
+import os
+import pandas as pd
+
+DATA_PATH = "data/humpback-whale-identification/train/"
 
 
 class StartingDataset(torch.utils.data.Dataset):
@@ -6,14 +12,23 @@ class StartingDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, df): 
+        self.images = df['Image']
+        self.labels = df['Id']
+        print(self.images)
 
     def __getitem__(self, index):
+        print("index: " + str(index))
+        image = Image.open(DATA_PATH + self.images[index])
+        image_tensor = transforms.ToTensor()(image)
+        # TODO: reshape image_tensor
+        label = self.labels[index]
+        return image_tensor, label
+        '''
         inputs = torch.zeros([3, 224, 224])
         label = 0
 
         return inputs, label
-
+        '''
     def __len__(self):
-        return 10000
+        return len(self.images)
