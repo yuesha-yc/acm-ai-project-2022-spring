85ad872a8379abb36799be2456eafb1fdd978ab4
yuesha-yc <kevinwangyichen@gmail.com>
2022-05-05 18:39:44 -0700

    Resize Data

--- data/ResizeData.py
+++ data/ResizeData.py
@@ -1,1 +1,13 @@
 from PIL import Image
+
+def resize_image(df, size):
+    """
+    Resizes all images in a dataframe to a specified size.
+    """
+    for i, id in enumerate(df['Image']):
+        if (i % 1000 == 0):
+            print(i, id)
+        image = Image.open(data_path + id)
+        image = image.convert('RGB')
+        image = image.resize(size)
+        
